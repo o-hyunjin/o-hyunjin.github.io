be6967b76d2da705a551a721de30eee89dac101d
o-hyunjin <82138573+o-hyunjin@users.noreply.github.com>
2021-04-25 01:07:41 +0900

    Update jin.js

--- jin.js
+++ jin.js
@@ -1,7 +1,7 @@
 
 
         var slideIndex = 1;
-        
+        showSlides(slideIndex);
         
         function plusSlides(n) {
           showSlides(slideIndex += n);
@@ -23,6 +23,7 @@
           for (i = 0; i < dots.length; i++) {
               dots[i].className = dots[i].className.replace(" active", "");
           }
-           
+           slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
           
         }
